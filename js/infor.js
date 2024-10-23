c00f6e485c73156847f8a49fd6bc6531ac0c733a
ARPHAM <oaichua42@gmail.com>
2024-10-23 15:41:26 +0700

    update_about_us

--- js/infor.js
+++ js/infor.js
@@ -6,34 +6,28 @@ window.addEventListener("resize", function () {
 });
 let isHovering1 = false;
 let isHovering2 = false;
-divprofile_card.style.display = "none";
 div3_7.addEventListener("mouseover", () => {
   isHovering1 = true;
   divprofile_card.style.display = "block";
-  console.log(1);
 });
 
 divprofile_card.addEventListener("mouseover", () => {
   isHovering2 = true;
   divprofile_card.style.display = "block";
-  console.log(2);
 });
 
 div3_7.addEventListener("mouseout", () => {
   isHovering1 = false;
   checkAndHide();
-  console.log(3);
 });
 
 divprofile_card.addEventListener("mouseout", () => {
   isHovering2 = false;
   checkAndHide();
-  console.log(4);
 });
 
 function checkAndHide() {
   if (!isHovering1 && !isHovering2) {
     divprofile_card.style.display = "none";
-    console.log(5);
   }
 }
